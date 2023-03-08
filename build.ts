acd5c3824d0d67b3ee1068c4ab5ae524d8e4aa5a
zys8119 <770959294@qq.com>
2023-03-08 14:53:23 +0800

    [add] 支持重写route

--- build.ts
+++ build.ts
@@ -1,11 +1,22 @@
 import {buildSync} from "esbuild"
 import fastGlob from "fast-glob"
+import fsExtra from "fs-extra"
+import {parse, resolve} from "path"
+const {writeFileSync, mkdirsSync} = fsExtra
 
-buildSync({
+const {outputFiles} = buildSync({
     entryPoints:fastGlob.sync("./src/**/*", {absolute:true}),
     outdir:'dist',
-    platform:"node",
-    loader:{
-        '.ts':'ts'
-    },
+    write:false
+})
+outputFiles.forEach(({path, text})=>{
+    const {name, dir} = parse(path)
+    mkdirsSync(dir)
+    writeFileSync(resolve(path, `../${name}.js`), text)
+    writeFileSync(resolve(path, `../${name}.mjs`), text)
+    writeFileSync(resolve(path, `../${name}.cjs`), text
+        .replace(/^(\s*import)(\s*\*\s*as\s*)(.*)(\s*?from)(.*)(;)/img, '$1 $3$4$5$6')
+        .replace(/^(\s*)(import)(.*)(from)(.*)(;)/img, '$1const$3 = require($5)$6')
+        .replace(/^(export\s*)(default)(\s*.*)/img, `module.exports = $3\nmodule.exports.default = $3`)
+    )
 })
---
+++ dist/vite.mjs
@@ -0,0 +1,50 @@
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
+import * as sfc from "@vue/compiler-sfc";
+import glob from "fast-glob";
+import _ from "lodash";
+const ROUTES_META = function(config) {
+  const currConfig = _.merge({
+    route: "route",
+    route_suffix: "ts",
+    suffix: ["vue", "jsx", "tsx"],
+    views: "views",
+    pageJson: "page.json",
+    routesFilter: /\/alert\//
+  }, config);
+  const fileReg = new RegExp(`\\.(${currConfig.suffix.join("|")})$`);
+  const routeReg = new RegExp(`${currConfig.route}.${currConfig.route_suffix}$`);
+  const routePath = resolve(process.cwd(), currConfig.route + "." + currConfig.route_suffix);
+  const cwd = resolve(process.cwd(), "node_modules", "vitejs-plugin-vue-route-auto-import");
+  const routeCode = currConfig.route_suffix === "ts" ? readFileSync(resolve(cwd, "src/route.ts"), "utf-8") : readFileSync(resolve(cwd, "dist/route.js"), "utf-8");
+  writeFileSync(routePath, routeCode);
+  const ROUTES_CUSTOM_ROUTER = currConfig.routes_extend ? `import ROUTES_CUSTOM_ROUTER from "${currConfig.routes_extend}"` : "const ROUTES_CUSTOM_ROUTER = []";
+  const ROUTES_CUSTOM_CONFIG = currConfig.routesCustomConfig ? `import ROUTES_CUSTOM_CONFIG from "${currConfig.routesCustomConfig}"` : "const ROUTES_CUSTOM_CONFIG = {}";
+  let routeModuleId = null;
+  return {
+    name: "ROUTES_META",
+    enforce: "pre",
+    load(id) {
+      if (routeReg.test(id)) {
+        routeModuleId = id;
+        const map = glob.sync("**/*.vue", { absolute: true }).reduce((res, id2) => {
+          const code = readFileSync(id2, "utf-8");
+          const descriptor = sfc.parse(`${code.match(/<script([^>])*>/)?.[0] || "<script>"}console.log(1)<script/>`).descriptor;
+          return {
+            ...res,
+            [id2.replace(process.cwd(), ".")]: descriptor?.scriptSetup?.attrs
+          };
+        }, {});
+        return `${ROUTES_CUSTOM_ROUTER}
+${ROUTES_CUSTOM_CONFIG}
+` + readFileSync(id, "utf-8").replace(/ROUTES_META/img, JSON.stringify(map)).replace(/ROUTES_FILTER_REG/img, new RegExp(currConfig.routesFilter).toString()).replace(/views/img, currConfig.views).replace(/page\.json/img, currConfig.pageJson).replace(/\{vue\,jsx\,tsx\}/img, `{${currConfig.suffix.join(",")}}`).replace(/\(vue\|jsx\|tsx\)/img, `(${currConfig.suffix.join("|")})`);
+      }
+    },
+    handleHotUpdate(cxt) {
+      if (fileReg.test(cxt.file)) {
+        cxt.server.reloadModule(cxt.server.moduleGraph.getModuleById(routeModuleId));
+      }
+    }
+  };
+};
+export default ROUTES_META;
